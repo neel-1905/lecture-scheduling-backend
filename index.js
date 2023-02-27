b163e77aa7595a8a8badafade94bf3c8c98e30f5
neel-1905 <neelnarwadkar@gmail.com>
2023-02-27 17:19:32 +0530

    env

--- index.js
+++ index.js
@@ -12,11 +12,7 @@ const lectureRoute = require("./routes/lectures");
 dotenv.config();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "https://master--lucky-yeot-321d71.netlify.app",
-  })
-);
+app.use(cors());
 
 app.use("/users", userRoute);
 app.use("/courses", courseRoute);
